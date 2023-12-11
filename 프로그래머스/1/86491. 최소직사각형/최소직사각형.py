a741ad07500393cd73354cb61067fea8f8b1b838
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2023-12-11 22:15:14 +0900

    [level 1] Title: 최소직사각형, Time: 8.36 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,12 +1,19 @@
 def solution(sizes):
-    answer = 0
-    w_lst, h_lst = [], []    
+    # 가로, 세로 최대 높이
+    w_max, h_max = 0, 0
+    w_lst = []
+    h_lst = []
 
+    # 모든 명함의 가로, 세로에 대해
     for w, h in sizes:
+        # 현재 명함의 가로, 세로중 큰 값을 저장후 w_max와 비교해 큰값 저장
         w_lst.append(max(w, h))
+
+        # 현재 명함의 가로, 세로중 작은 값을 저장후 h_max 와 비교해 큰값 저장
         h_lst.append(min(w, h))
-    
+
+    # 가로 = 큰 값중 큰값 / 세로 = 작은값중 큰값
     answer = max(w_lst)*max(h_lst)
-        
 
     return answer
+
