927804266155588d29e1c36507007aa4730641e6
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2023-12-29 03:02:06 +0900

    [Gold V] Title: 두 용액, Time: 152 ms, Memory: 47052 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())    # 용액의 수
+lst = list(map(int, input().split()))   # 용액의 특성값  / 양수라면 산성, 음수라면 알칼리성
+Min = 10**10
+ans = []    # 합이 0에 근접한 두 용액을 저장할 리스트
+
+lst.sort()
+
+L = 0
+R = N-1
+
+while L < R:
+    Sum = lst[L] + lst[R]
+    if abs(Min) > abs(Sum):
+        Min = Sum
+        ans.append([L, R])
+
+    if Sum > 0:
+        R -= 1
+    elif Sum < 0:
+        L += 1
+    elif Sum == 0:
+        break
+
+print(lst[ans[-1][0]], lst[ans[-1][1]])
