ecc5306b88082d56fa420b8db92974353b3f9ea9
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-01-11 04:52:30 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,12 +1,9 @@
 N = int(input())
 
-dp = [[0]*10 for _ in range(N+1)]
-
-for i in range(10):
-    dp[1][i] = 1
+dp2 = [1]*10
 
 for i in range(2, N+1):
-    for j in range(0, 10):
-        dp[i][j] = sum(dp[i-1][j:])
+    for j in range(10):
+        dp2[j] = sum(dp2[j:])
 
-print(sum(dp[N])%10007)
+print(sum(dp2)%10007)
