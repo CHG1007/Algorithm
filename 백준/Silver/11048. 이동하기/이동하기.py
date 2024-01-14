531b4f142dad375439b4e61115402d09f2d8cb96
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-01-14 16:35:58 +0900

    [Silver II] Title: 이동하기, Time: 716 ms, Memory: 77960 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,14 @@
+import sys
+intput = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr = [[0]*(M+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0]*(M+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = max(dp[i][j-1], dp[i-1][j]) + arr[i][j]
+
+print(dp[i][j])
