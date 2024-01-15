64775bc5faff98ce39d4840c651bde41dd5259cc
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-01-15 21:38:30 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -4,15 +4,20 @@ input = sys.stdin.readline
 N = int(input())
 arr = [[0]*(N+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
 
+# dp[i][j] -> (1,1)에서 시작하여 (i,j)로 오는 경로 수
 dp = [[0]*(N+1) for _ in range(N+1)]
 dp[1][1] = 1
 
 for i in range(1, N+1):
     for j in range(1, N+1):
-        if dp[i][j] != 0 and arr[i][j] != 0:
-            if i+arr[i][j] <= N:
-                dp[i+arr[i][j]][j] += dp[i][j]
+        jump = arr[i][j]
+        # 시작점부터 갈수있는 위치만 탐색
+        if dp[i][j] != 0 and jump != 0:
+            # 열 범위 내라면 -> 우측 이동
+            if i+jump <= N:
+                dp[i+jump][j] += dp[i][j]
+            # 행 범위 내라면 -> 아래 이동
             if j + arr[i][j] <= N:
-                dp[i][j+arr[i][j]] += dp[i][j]
+                dp[i][j+jump] += dp[i][j]
 
 print(dp[N][N])
