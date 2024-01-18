59437bfd9cd29edb6800f54ccd97a1747314fda4
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-01-18 23:16:58 +0900

    [Silver I] Title: 카잉 달력, Time: 1280 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -4,14 +4,18 @@ input = sys.stdin.readline
 
 # 정답 반환 함수
 def cal(x, y):
-    k = x
+    cnt = max(M,N)
+    
+    if M > N:
+        k = x
+    else:
+        k = y
     while k <= M*N:
-        if (k-y)%N == 0:
+        if (k-x)%M == 0 and (k-y)%N == 0:
             return k
-        k += M
+        k += cnt
 
     return -1
-
 
 T = int(input())
 for _ in range(T):
