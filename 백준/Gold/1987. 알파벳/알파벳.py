e8930838d8bd342b49b2925c7a7a9bcadc9a7650
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-01-21 17:34:56 +0900

    [Gold IV] Title: 알파벳, Time: 4316 ms, Memory: 152476 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,16 +1,12 @@
-import sys
-input = sys.stdin.readline
-
-
 def dfs(si, sj, n):
     global ans
 
     for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
         ni, nj = si+di, sj+dj
-        if 0<=ni<R and 0<=nj<C and v[ord(arr[ni][nj])] == 0:
-            v[ord(arr[ni][nj])] = 1
+        if 0<=ni<R and 0<=nj<C and v[ord(arr[ni][nj])-A] == 0:
+            v[ord(arr[ni][nj])-A] = 1
             dfs(ni, nj, n+1)
-            v[ord(arr[ni][nj])] = 0
+            v[ord(arr[ni][nj])-A] = 0
 
     ans = max(ans, n)
 
@@ -18,8 +14,9 @@ def dfs(si, sj, n):
 R, C = map(int, input().split())
 
 arr = [input() for _ in range(R)]
-v = [0]*100
-v[ord(arr[0][0])] = 1
+A = ord('A')
+v = [0]*A
+v[ord(arr[0][0])-A] = 1
 ans = 0
 
 dfs(0, 0, 1)
