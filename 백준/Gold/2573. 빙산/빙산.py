0c2afecaa2f171f2223cf7c7d2a19b2ec42f4b64
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-02-02 16:36:05 +0900

    [Gold IV] Title: 빙산, Time: 3624 ms, Memory: 34480 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -2,58 +2,70 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
+
+def solve():
+    # 빙하 최대 높이인 10번(년) 동안 반복
+    for Year in range(1, 90000):
+        cnt = 0
+        v = [[0] * M for _ in range(N)]
+
+        # 1. 1년마다 빙하 높이 감소 좌표마다 계산해두기
+        for i in range(N):
+            for j in range(M):
+                if arr[i][j] != 0:
+                    year(i, j)
+
+        # 2. 1에서 계산한 높이 만큼 빙하 높이 감소시키기
+        for i in range(N):
+            for j in range(M):
+                if arr2[i][j] > 0:
+                    arr[i][j] = max(0, arr[i][j] - arr2[i][j])
+                    arr2[i][j] = 0
+
+        # 3. 빙하 몇 덩어리 인지 계산하기
+        for i in range(N):
+            for j in range(M):
+                if arr[i][j] != 0 and v[i][j] == 0:
+                    bfs(i, j, v)
+                    cnt += 1
+                    # 2덩어리 이상이라면 종료
+                    if cnt > 1:
+                        return Year
+
+        # 4. 다 녹아서 0 덩어리이면 종료
+        if cnt == 0:
+            return 0
+
+
+# 1년후 현재 좌표의 빙하 감소 높이 구하는 함수
+def year(ci, cj):
+    for di, dj in ((-1,0),(1,0),(0,-1),(0,1)):
+        ni, nj = ci+di, cj+dj
+        if arr[ni][nj] == 0:
+            arr2[ci][cj] += 1
+
+
 def bfs(si, sj, v):
+    # 1. 큐 생성
     q = deque()
 
-    q.append((si,sj))
-    v[si][sj]=1
+    # 2. 초기데이터 입력
+    q.append([si, sj])
+    v[si][sj] = 1
 
+    # 3. 4방향 탐색
     while q:
-        ci,cj = q.popleft()
-        # 네방향, 범위내(X), 미방문, >0
-        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni,nj = ci+di, cj+dj
-            if v[ni][nj]==0 and arr[ni][nj]>0:
-                q.append((ni,nj))
-                v[ni][nj]=1
+        ci, cj = q.popleft()
 
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ni, nj = ci + di, cj + dj
+            if arr[ni][nj] != 0 and v[ni][nj] == 0:
+                v[ni][nj] = 1
+                q.append([ni, nj])
 
-def solve(): # 1 ~ 900000 년, 전체순회 반복작업
-    for year in range(1, 900000):
-        # [1] 네방향 0의 개수 카운트
-        a_sub = [[0]*M for _ in range(N)]
-        for i in range(1, N-1):
-            for j in range(1, M-1):
-                if arr[i][j]==0:
-                    continue
-                for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-                    ni,nj = i+di, j+dj
-                    if arr[ni][nj]==0:
-                        a_sub[i][j]+=1
-
-        # [2] 높이 낮추기
-        for i in range(1, N-1):
-            for j in range(1, M-1):
-                if a_sub[i][j]>0:
-                    arr[i][j]=max(0, arr[i][j]-a_sub[i][j])
-
-        # [3] 빙산 덩어리 개수 카운트
-        v = [[0]*M for _ in range(N)]
-        cnt=0
-        for i in range(1, N-1):
-            for j in range(1, M-1):
-                if v[i][j]==0 and arr[i][j]>0:
-                    bfs(i, j, v)
-                    cnt+=1
-                    if cnt>1: # 두 덩어리 이상...
-                        return year
-        if cnt==0:  # 덩어리개수 0개이면.. stop
-            return 0
-    # 이자리에 올 일은 없지만...
-    return -1
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+arr2 = [[0] * M for _ in range(N)]
 
-ans = solve()
-print(ans)
+print(solve())
