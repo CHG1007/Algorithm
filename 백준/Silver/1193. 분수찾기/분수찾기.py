bd38e48f8532c04722722510357f8ed92cae56e4
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-02-20 23:00:48 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+line = 0
+
+while N > line:
+    N -= line
+    line += 1
+
+if line%2 == 0:
+    a = N
+    b = line-N+1
+else:
+    a = line-N+1
+    b = N
+
+print(f'{a}/{b}')
