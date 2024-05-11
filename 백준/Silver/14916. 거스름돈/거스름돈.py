1a57b76fc982beb0f5d735fa77b1ea662caf8aec
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-05-11 23:04:50 +0900

    [Silver V] Title: 거스름돈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+ans = 0
+c2, c5 = 0, 0
+
+if (n%5)%2 == 0:
+    ans += n//5
+    ans += (n%5)//2
+else:
+    if n < 4:
+        ans = -1
+    else:
+        ans += n//5 - 1
+        ans += ((n%5)+5)//2
+
+print(ans)
