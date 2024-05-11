d1c7b2800ef77c2081085d84fb340435d7ff5f84
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-05-11 22:49:47 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -6,16 +6,16 @@ n = int(input())
 lst = [0] + list(map(int, input().split()))
 
 # dp[i]: i번째를 포함하는 부분 수열 중 합의 최대값(초기값=본인값)
-dp = [0]*(n+1)
+dp = []
+for num in lst:
+    dp.append(num)
 
 for i in range(1, n+1):
     # i번째 수보다 전에있는 수들중에 i번째 수보다 크기가 작다면 Sum에 누적합 계산
     for j in range(i):
         if lst[i] > lst[j]:
             dp[i] = max(dp[i], dp[j]+lst[i])
-        else:
-            dp[i] = max(dp[i], lst[i])
-        
+
 # print(*lst, sep='\t')
 # print(*dp, sep='\t')
 print(max(dp))
