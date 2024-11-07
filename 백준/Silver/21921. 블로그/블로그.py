ceee108fb7eeece9364b411ce832e1516c48e175
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-11-08 01:19:40 +0900

    [Silver III] Title: 블로그, Time: 136 ms, Memory: 58784 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -12,7 +12,7 @@ max_visit, cnt = total, 1
 for i in range(1, n-(x-1)):
     # i일 기준 x일 동안 방문자 합
     total += lst[i+(x-1)] - lst[i-1]
-
+    
     if total == max_visit:      # 직전 최대 방문자 합과 같다면
         cnt += 1                # 기간 수 증가
     elif total > max_visit:     # 합이 더 크다면
