356aaa941baee95e9b44fad30bd56294761790eb
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-11-15 00:13:47 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,568 ms, Memory: 49,524 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,26 +1,23 @@
 import itertools
 
-t = int(input())
 
+t = int(input())
 for tc in range(t):
     n, l = map(int, input().split())
+    data = [list(map(int, input().split())) for _ in range(n)]
 
-    lst = [list(map(int, input().split())) for _ in range(n)]
-    ans = 0
+    max_score = 0
 
     for i in range(1, n+1):
-        # 재료를 1개부터 n개 까지 고르는 모든 경우의 수에 대해
-        for data in itertools.combinations(lst, i):
-            score, kal = 0, 0
-            # 점수와 칼로리 누적 합산
-            for value in data:
-                kal += value[1]
-                score += value[0]
-                # 칼로리가 제한값을 넘지 않는다면
-                if kal > l:
-                    continue
-                # 점수합산 최댓값 갱신
-                if score > ans:
-                    ans = score
+        for value in itertools.combinations(data, i):
+            kcal = 0
+            score = 0
+            for v in range(len(value)):
+                kcal += value[v][1]
+                score += value[v][0]
+            if kcal > l:
+                continue
+            if max_score < score:
+                max_score = score
 
-    print(f'#{tc+1} {ans}')
+    print(f'#{tc+1} {max_score}')
