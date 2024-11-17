f25fc6a94333cd92003bc0104f4b5f8dd625286a
CHG1007 <150348562+CHG1007@users.noreply.github.com>
2024-11-17 11:49:12 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,20 +1,24 @@
+#   N과 M (6)    실버 3
+
+
 def dfs(n, s, tlst):
-    if n==M:
+    if n == m:
         ans.append(tlst)
         return
 
-    for j in range(s, N):   # 오름차순 수열을 위해서 선택한 숫자 이후 값부터 선택
-        if v[j]==0:
-            v[j]=1
+    for j in range(s, N):
+        if v[j] == 0:
+            v[j] = 1
             dfs(n+1, j+1, tlst+[lst[j]])
-            v[j]=0
+            v[j] = 0
 
-N, M = map(int, input().split())
-lst = sorted(list(map(int, input().split())))
+
+N, m = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
 
 ans = []
 v = [0]*N
-# 선택개수, 시작j위치, tlst
 dfs(0, 0, [])
 
 for lst in ans:
