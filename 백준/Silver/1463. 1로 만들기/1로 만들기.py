c50cb089b157e983ad516fdc332676bd9cd869cb
HongGwon Choi <150348562+CHG1007@users.noreply.github.com>
2025-05-24 01:17:28 +0900

    [Silver III] Title: 1로 만들기, Time: 496 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,15 @@
-N = int(input())
+#   1로 만들기 실버 3
 
-dp = [0]*(N+1)  # dp[1]=0   초기값 설정
 
-for i in range(2, N+1):
+n = int(input())
+dp = [0]*(n+1)
+
+for i in range(2, n+1):
     dp[i] = dp[i-1] + 1
-    if i % 2 == 0:    # 숫자가 2의 배수인 경우
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i % 3 == 0:    # 숫자가 3의 배수인 경우
-        dp[i] = min(dp[i], dp[i//3]+1)
 
-ans = dp[N]
-print(ans)
+    if i%2 == 0:
+        dp[i] = min(dp[i//2] + 1, dp[i])
+    if i%3 == 0:
+        dp[i] = min(dp[i//3] + 1, dp[i])
+
+print(dp[n])
