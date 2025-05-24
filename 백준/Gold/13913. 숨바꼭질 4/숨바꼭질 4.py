9e3f6403245a6248b88172268e016653dd5c8c2a
HongGwon Choi <150348562+CHG1007@users.noreply.github.com>
2025-05-24 11:28:39 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 144 ms, Memory: 41328 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -3,44 +3,42 @@ import sys
 input = sys.stdin.readline
 
 
-# e: 종료지점, n: 이동횟수
-def move(e, n):
-    ans = []
+def move(lst, target, cnt):
+    ans = [target]  # 이동 경로 리스트(역순)
+    tmp = target    # 경로 확인 변수
+    
+    # 이동 횟수 만큼 이동 경로 ans에 저장
+    for _ in range(cnt):
+        tmp = lst[tmp]
+        ans.append(tmp)
+    
+    print(*ans[::-1])   # 역순 출력
 
-    temp = e
-    for i in range(n):
-        ans.append(temp)
-        temp = v2[temp]
 
-    print(*ans[::-1])
-
-
-def bfs(s, e):
-    global v2
-    # 1. 큐, 방문 배열 선언
+def bfs(si, ei):
+    # 1. 큐 생성, 방문 배열, 경로 배열 생성
     q = deque()
-    v = [0]*200001
-    v2 = [0]*200001
+    v = [0]*(100001)
+    path = [0]*(100001)
 
-    # 2. 초기값, 방문 배열 초기화
-    q.append(s)
-    v[s] = 1
+    # 2. 초기 데이터 삽입, 방문 처리
+    q.append(si)
+    v[si] = 1
 
+    # 3. 3방향 탐색, 범위 10만, 방문 여부
     while q:
-        c = q.popleft()
-        # 수빈이 좌표 찾으면 종료 조건
-        if c == e:
-            print(v[e] - 1)
-            move(c, v[e])
+        ci = q.popleft()
+        if ci == ei:
+            print(v[ci]-1)
+            move(path, ci, v[ci]-1)
             return
 
-        # 3방향 탐색
-        for n in (c-1, c+1, c*2):
-            if 0 <= n <= 200000 and v[n] == 0:
-                q.append(n)
-                v[n] = v[c] + 1
-                v2[n] = c
+        for ni in ((ci-1), (ci+1), (2*ci)):
+            if 0<=ni<=100000 and v[ni] == 0:
+                q.append(ni)
+                v[ni] = v[ci] + 1
+                path[ni] = ci
 
 
-n, k = map(int, input().split())    # 수빈이 위치, 동생 위치
+n, k = map(int, input().split())
 bfs(n, k)
