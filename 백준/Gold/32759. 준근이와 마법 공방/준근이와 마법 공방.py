60ca871c65fb3430ef1c411464f5711ec70e3e96
HongGwon Choi <150348562+CHG1007@users.noreply.github.com>
2025-06-30 01:28:19 +0900

    [Gold IV] Title: 준근이와 마법 공방, Time: 452 ms, Memory: 42700 KB -BaekjoonHub

--- 백준/Gold/32759. 준근이와 마법 공방/준근이와 마법 공방.py
+++ 백준/Gold/32759. 준근이와 마법 공방/준근이와 마법 공방.py
@@ -4,7 +4,6 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
 lst.sort(reverse=True)
-
 MOD = 10**9+7   # 출력 시 사용할 모듈값 (문제 조건: p × q + r 중 r만 출력)
 
 # 1,2번째로 큰 마력석
@@ -15,7 +14,7 @@ for _ in range(n):
     # 가장 큰 두 마력석을 합성하여 새 마력석 생성
     newA = maxA + secondMaxA
 
-    # 가장 큰 값이 0이거나 음수면 앞으로 생성된 마력석은 그보다 작으므로 계산 불필요
+    # 가장 큰 값이 음수인 경우(0포함) 앞으로 생성된 마력석은 그보다 작으므로 계산 불필요
     if maxA <= 0:
         break
     else:
@@ -27,10 +26,10 @@ for _ in range(n):
         elif secondMaxA < newA:
             secondMaxA = newA
 
-        # 오버플로우 방지를 위해 각 값을 모듈러 처리
+        # 각 값을 모듈러 처리
         if maxA > MOD and secondMaxA > MOD:
-            maxA %= MOD
-            secondMaxA %= MOD
+            maxA = maxA % MOD
+            secondMaxA = secondMaxA % MOD
 
 # 마지막에 생성된 마력석의 마나 수치를 출력
 # 단, 음수가 될 수 있으므로 MOD 더한 후 % MOD 처리
