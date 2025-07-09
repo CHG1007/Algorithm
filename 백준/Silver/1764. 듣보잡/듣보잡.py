6c887c304b8543cec952a0c71faac0ec5fc462c9
HongGwon Choi <150348562+CHG1007@users.noreply.github.com>
2025-07-10 00:03:23 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 43168 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,16 +1,17 @@
-import sys
+#   듣보잡 실버 4
 
+import sys
 input = sys.stdin.readline
 
+# 입력 처리
 n, m = map(int, input().split())
 
 # set 사용
 heard = set(input().strip() for _ in range(n))
-seen = set(input().strip() for _ in range(m))
+seen = set(input().strip() for _ in range(n))
 
 # 교집합
-result = sorted(heard & seen)
+ans = sorted(heard & seen)
 
 # 출력
-print(len(result))
-print("\n".join(result))
+print(len(ans), *ans, sep='\n')
