755bb3c3af40c6043ebbb52ae90c8489353a69b9
HongGwon Choi <150348562+CHG1007@users.noreply.github.com>
2025-07-10 23:47:53 +0900

    [Silver IV] Title: 수들의 합 2, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,45 +1,27 @@
-#   수들의 합 2 실버 4
-
-
-def prefix():
-    L = -1
-    R = 0
-    ans = 0
-
-    for i in range(1, n):
-        prefix_lst[i] = lst[i] + prefix_lst[i - 1]
-
-    while R < n:
-        total = prefix_lst[R] - prefix_lst[L]
-        if L == -1:
-            total = prefix_lst[R]
-        if total == m:
-            ans += 1
-            L += 1
-        elif total > m:
-            L += 1
-        elif total < m:
-            R += 1
-
-    return ans
+#   수들의 합 2 실버 4    개선 버전
 
 
 # 입력 처리
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
 
-prefix_lst = [0]*n
-prefix_lst[0] = lst[0]
+left = 0
+right = 0
+current_sum = 0
 ans = 0
 
-if n == 1:
-    if lst[0] == m:
-        ans = 1
-else:
-    ans = prefix()
+while True:
+    # 누적합이 목표 미만이면, 우측값 더하고, 우측 포인터 한칸 증가
+    if current_sum < m:
+        if right == n:
+            break
+        current_sum += lst[right]
+        right += 1
+    # 누접합이 목표 이상이면, 정답체크, 왼쪽 값을 빼고, 왼쪽 포인터 한칸 증가
+    else:
+        if current_sum == m:
+            ans += 1
+        current_sum -= lst[left]
+        left += 1
 
 print(ans)
-
-
-
-
